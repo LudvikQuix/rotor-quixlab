65926304efa9361d3cdf9980a79d59ba8f297042
Ludvik . <ludvik@quix.io>
2026-07-30 07:42:04 +0000

    Update file quixlab/rotor_analasys.py

--- quixlab/rotor_analasys.py
+++ quixlab/rotor_analasys.py
@@ -81,5 +81,10 @@ def kb_ballancing_machine_md():
     pass
 
 
+@canvas.file(position=(1821, -6023), size=(200, 200), code_height=0, path='Pffefeir-KB.py')
+def Pffefeir_KB():
+    pass
+
+
 if __name__ == "__main__":
     canvas.serve()
