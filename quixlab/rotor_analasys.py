96c7c2eb7c277f4ddbca2a7aec69aa01cc8dbc36
Ludvik . <ludvik@quix.io>
2026-07-30 14:51:52 +0000

    Update file quixlab/rotor_analasys.py

--- quixlab/rotor_analasys.py
+++ quixlab/rotor_analasys.py
@@ -125,7 +125,7 @@ def hochlauf():
     """)
 
 
-@canvas.notebook(position=(1821, -6873), size=(1461, 1544), code_height=200, viz={'cells': {'3': {'type': 'table', 'x': 'source', 'y': 'rows'}, '4': {'type': 'table', 'x': 'source', 'y': 'rows'}, '5': {'type': 'bar', 'x': 'amplitude_range', 'y': 'good_neutral_shaft_count'}}, 'outputCells': [3, 4], 'type': 'scatter', 'x': 'idx', 'y': ['good_neutral', 'bad']})
+@canvas.notebook(position=(1815, -6873), size=(1461, 1544), code_height=200, viz={'cells': {'3': {'type': 'table', 'x': 'source', 'y': 'rows'}, '4': {'type': 'table', 'x': 'source', 'y': 'rows'}, '5': {'type': 'bar', 'x': 'amplitude_range', 'y': 'good_neutral_shaft_count'}}, 'outputCells': [3, 4], 'type': 'scatter', 'x': 'idx', 'y': ['good_neutral', 'bad']})
 def rotor_data_reader():
     # %%
     df_optimierung = ql.sql("""
@@ -273,6 +273,7 @@ def rotor_data_reader():
     hist_df = hist_df.reset_index(drop=True)
 
     hist_df
+    # %%
 
 
 if __name__ == "__main__":
