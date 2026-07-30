7a156c0d94525962599d32ed546b063db1267dd7
Ludvik . <ludvik@quix.io>
2026-07-30 07:59:32 +0000

    Update file quixlab/rotor_analasys.py

--- quixlab/rotor_analasys.py
+++ quixlab/rotor_analasys.py
@@ -76,7 +76,7 @@ def optimierung():
     LIMIT 100""")
 
 
-@canvas.file(position=(-75, -6524), size=(520, 460), code_height=0, path='kb/ballancing-machine.md')
+@canvas.file(position=(-63, -6512), size=(520, 460), code_height=0, path='kb/ballancing-machine.md')
 def kb_ballancing_machine_md():
     pass
 
